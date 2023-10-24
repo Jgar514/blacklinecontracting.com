749b887a3247b7c9a372863a5a6040a2c0564a58
Joshua Garvey <j.garvey514@yahoo.com>
2023-10-23 22:37:37 -0400

    fixed conflicting css

--- blackline/src/components/Header.jsx
+++ blackline/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header({ title }) {
 			<div className=" container mx-auto">
 				<div className="flex items-center justify-center ">
 					<div className="bg-white/40 w-full md:w-10/12 lg:w-10/12 px-4 py-16 text-center flex flex-col gap-y-2 mt-8">
-						<h1 className="   text-black underline text-2xl md:text-4xl lg:text-7xl font-bold text-3xl">{title}</h1>
+						<h1 className="   text-black underline  md:text-4xl lg:text-7xl font-bold text-3xl">{title}</h1>
 					</div>
 				</div>
 			</div>
